97fc066f0255a75aa4cbfee7eebd36f8f0b978ad
angelo.pereira <angelo.pereira@basis.com.br>
2022-04-09 20:17:44 -0300

    Melhoria na geração do equal e hashcode

--- src/main/java/br/com/vr/miniautorizador/model/Cartao.java
+++ src/main/java/br/com/vr/miniautorizador/model/Cartao.java
@@ -1,11 +1,7 @@
 package br.com.vr.miniautorizador.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
 
@@ -18,10 +14,12 @@ import java.math.BigDecimal;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Cartao implements Serializable {
 
     @Id
+    @EqualsAndHashCode.Include
     @Size(min = 16, max = 16, message = "O cartão deve ser composto por 16 digitos numéricos sem espaço ou traços ex: 1234123412341234")
     private String numeroCartao;
 
