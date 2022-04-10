85efa595c14361a9499d8c8c980aa999fd9d7418
angelo.pereira <angelo.pereira@basis.com.br>
2022-04-10 17:34:54 -0300

    Documentação API Transação

--- src/main/java/br/com/vr/miniautorizador/controller/TransacaoController.java
+++ src/main/java/br/com/vr/miniautorizador/controller/TransacaoController.java
@@ -1,7 +1,15 @@
 package br.com.vr.miniautorizador.controller;
 
+import br.com.vr.miniautorizador.enums.TipoRetornoErroEnum;
+import br.com.vr.miniautorizador.model.CartaoProjection;
 import br.com.vr.miniautorizador.model.Transacao;
 import br.com.vr.miniautorizador.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Tag(name = "transacoes", description = "Endpoint para as operações de transação")
 @RestController
 @RequestMapping("/transacoes")
 public class TransacaoController {
@@ -20,6 +29,15 @@ public class TransacaoController {
         this.service = service;
     }
 
+    @Operation(summary = "Realiza uma operação no cartão")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Caso a operação seja realizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Caso o cartão não seja encontrado"),
+            @ApiResponse(responseCode = "422", description = "Caso ocorra algum erro na transação",
+                    content = {@Content(mediaType = "text/plain", schema = @Schema(implementation = TipoRetornoErroEnum.class)),
+                    })
+    }
+    )
     @PostMapping
     public ResponseEntity<?> realizarTransacao(@Valid @RequestBody Transacao transacao) {
         service.realizarTransacao(transacao);
